5ea3ed7e1ef755cd84228945fd02480aa38d5bbb
specifics7 <113580708+specifics7@users.noreply.github.com>
2025-10-16 14:43:17 -0700

    Dynamically set grid size

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const container = document.getElementById('etch-container');
+const btn = document.querySelector('.grid-btn');
 
-for (let i = 0; i < 16 * 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-square');
+function drawGrid(gridSize) {
+    // Set CSS variable for grid size
+    container.style.setProperty('--grid-size', gridSize);
 
-    div.addEventListener('mouseover', () => {
-        div.classList.add('hovered');
-    });
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid-square');
 
-    container.appendChild(div);
-
+        div.addEventListener('mouseover', () => {
+            div.classList.add('hovered');
+        });
+        container.appendChild(div);
+    }
 }
+
+btn.addEventListener('click', () => {
+    let gridSize = prompt("Please choose your grid size (1-100)");
+    console.log("Test");
+
+    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert("Invalid input, please try again");
+        return;
+    }
+
+    container.innerHTML = '';
+    drawGrid(gridSize);
+});
+
+drawGrid(16);
