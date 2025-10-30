bcd3c2d0ab274fafbee57d957f063e8c991c12da
specifics7 <113580708+specifics7@users.noreply.github.com>
2025-10-29 20:56:18 -0700

    Add rainbow brush, turns divs into random color

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const container = document.getElementById('etch-container');
 const btnReset = document.querySelector('.reset-btn');
+const btnRainbow = document.getElementById('rainbow-toggle')
 const gridSlider = document.getElementById('grid-slider');
 const gridSizeDisplay = document.getElementById('grid-size-display');
+
+let isRainbow = false;
 let currGridSize = 16;
 
 function drawGrid(gridSize) {
@@ -10,13 +13,16 @@ function drawGrid(gridSize) {
     container.style.setProperty('--grid-size', gridSize);
 
     for (let i = 0; i < gridSize * gridSize; i++) {
-        const div = document.createElement('div');
-        div.classList.add('grid-square');
+        const divSquare = document.createElement('div');
+        divSquare.classList.add('grid-square');
 
-        div.addEventListener('mouseover', () => {
-            div.classList.add('hovered');
+        divSquare.addEventListener('mouseover', () => {
+            divSquare.classList.add('hovered');
+            if (isRainbow === true) {
+                divSquare.style.backgroundColor = setColor(); 
+            }
         });
-        container.appendChild(div);
+        container.appendChild(divSquare);
     }
 }
 
@@ -25,9 +31,19 @@ function resetGrid() {
     drawGrid(currGridSize);
 }
 
+function setColor() {
+    let randColor = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + randColor;
+}
+
 btnReset.addEventListener('click', () => {
     resetGrid();
 })
+
+btnRainbow.addEventListener('click', () => {
+    isRainbow = !isRainbow;
+    btnRainbow.classList.toggle('active');
+});
 
 gridSlider.addEventListener('input', (e) => {
     const newSize = parseInt(e.target.value);
